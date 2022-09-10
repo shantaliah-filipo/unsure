c2f4488f54faaeedbfa667de4c6ddf632996750e
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-10 22:19:58 +1200

    Change Title and Heading on homepage

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <h1>Plan your trip to Rarotonga!</h1>
 
         <DateRangeSelector />
       </div>
