1555ee3210a0be24f7e4266219e5842894365906
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-10 21:01:17 +1200

    Initial setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,12 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
-import { fetchFruits } from '../actions'
-
+import DateRangeSelector from './DateRangeSelector'
 function App() {
-  const fruits = useSelector((state) => state.fruits)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [])
-
   return (
     <>
       <div className="app">
         <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {fruits.map((fruit) => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
+
+        <DateRangeSelector />
       </div>
     </>
   )
---
+++ client/components/DateRangeSelector.jsx
@@ -0,0 +1,11 @@
+import React, { useSEffect, useState } from 'react'
+
+function DateRangeSelector() {
+  return (
+    <div>
+      <h3>Hi</h3>
+    </div>
+  )
+}
+
+export default DateRangeSelector
