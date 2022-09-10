5e42fdfb7aeb73841339d95c12a17b9202537716
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-10 21:24:43 +1200

    Calendar renders on page and you can select date

--- client/components/DateRangeSelector.jsx
+++ client/components/DateRangeSelector.jsx
@@ -1,8 +1,30 @@
-import React, { useSEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Calendar, DateRange } from 'react-date-range'
+import format from 'date-fns/format'
+
+import { addDays } from 'date-fns'
+
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
 
 function DateRangeSelector() {
+  // date date
+  const [calendar, setCalendar] = useState('')
+
+  // on date change, store date in state
+  const handleSelect = (date) => {
+    // console.log(date)
+    console.log(format(date, 'dd/MM/yyyy'))
+    setCalendar(format(date, 'dd/MM/yyyy'))
+  }
+
   return (
-    <div>
+    <div className="calendarWrap">
+      <Calendar
+        date={new Date()}
+        onChange={handleSelect}
+        className="calendarElement"
+      />
       <h3>Hi</h3>
     </div>
   )
