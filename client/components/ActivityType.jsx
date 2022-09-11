0d00902755a59fdc9545ceec9e5a8c85b8a4a48f
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-11 20:42:08 +1200

    Display activity types on page

--- client/components/ActivityType.jsx
+++ client/components/ActivityType.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
-
+import { activities } from '../utils/activities'
 
 function ActivityType() {
   const [checkedState, setCheckedState] = useState()
-
 
   return (
     <div>
       <h3>Select the Activity Type</h3>
       <input type="checkbox" id="water-activities" />
       Water-Based Activtities
+      <>
+        {activities.map(({ type }, index) => {
+          return (
+            <li key={index}>
+              <input type="checkbox" />
+              {type}
+            </li>
+          )
+        })}
+      </>
     </div>
   )
 }
---
+++ client/utils/activities.js
@@ -0,0 +1,7 @@
+export const activities = [
+  { id: 1, type: 'Water-based' },
+  { id: 2, type: 'Land-based' },
+  { id: 3, type: 'Cultural' },
+  { id: 4, type: 'Nightlife' },
+  { id: 5, type: 'Cuisine' },
+]
