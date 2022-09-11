4219cfd997080c74cea9b23373fac4909b122a73
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-11 20:02:48 +1200

    Added a checkbox with a hard-coded value

--- client/components/ActivityType.jsx
+++ client/components/ActivityType.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 
+
 function ActivityType() {
+  const [checkedState, setCheckedState] = useState()
+
+
   return (
     <div>
       <h3>Select the Activity Type</h3>
+      <input type="checkbox" id="water-activities" />
+      Water-Based Activtities
     </div>
   )
 }
