6794ddb9a565dd9971cfabd12af250401d8f440e
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-11 18:12:42 +1200

    Create ActivityType file and import into app.jsx

---
+++ client/components/ActivityType.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function ActivityType() {
+  return (
+    <div>
+      <h3>Select the Activity Type</h3>
+    </div>
+  )
+}
+
+export default ActivityType
