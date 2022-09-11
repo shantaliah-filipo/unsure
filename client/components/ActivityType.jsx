5478bb65551e25810cb883dd25a9f62b1fe8b9b6
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-11 21:59:38 +1200

    Display activity types on page

--- client/components/ActivityType.jsx
+++ client/components/ActivityType.jsx
@@ -6,18 +6,36 @@ function ActivityType() {
 
   return (
     <div>
-      <h3>Select the Activity Type</h3>
-      <input type="checkbox" id="water-activities" />
-      Water-Based Activtities
+      <h3>Which Activities Interest You?</h3>
+      {/* <input type="checkbox" id="water-activities" />
+      Water-Based Activtities */}
       <>
-        {activities.map(({ type }, index) => {
-          return (
-            <li key={index}>
-              <input type="checkbox" />
-              {type}
-            </li>
-          )
-        })}
+        <form>
+          <ul className="activities-list">
+            {/* <div className="inputActivities "> */}
+            {activities.map(({ type }, index) => {
+              return (
+                <li key={index}>
+                  <div className="main">
+                    <div className="activities-list-item">
+                      <div className="left-section">
+                        <input
+                          type="checkbox"
+                          id={`custom-checkbox-${index}`}
+                          name={name}
+                          value={name}
+                        />
+                        {type}
+                        {/* <div className="right-section"></div> */}
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              )
+            })}
+            {/* </div> */}
+          </ul>
+        </form>
       </>
     </div>
   )
