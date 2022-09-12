b34c0e0461f7a2775a68a78d50fbf0709ac2abc9
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-12 18:37:57 +1200

    created post request for adding new activity

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,6 +1,9 @@
-import { getActivityTypes } from '../apis/activityTypes'
+import { getActivityTypes, addNewActivityType } from '../apis/activityTypes'
 
 export const SET_ACTIVITY_TYPE = 'SET_ACTIVITY_TYPE'
+export const SET_ACTIVITY_TYPES_PENDING = 'SET_ACTIVITY_TYPES_PENDING'
+export const SET_ACTIVITY_TYPES_SUCCESS = 'SET_ACTIVITY_TYPES_SUCCESS'
+export const SET_ERROR = 'SET_ERROR'
 
 // simple action
 export function setActivityType(activityType) {
@@ -10,11 +13,44 @@ export function setActivityType(activityType) {
   }
 }
 
-// thunk - e.g. fetchactivitytype
+// thunks - e.g. fetchactivitytype
 export function fetchActivityType() {
   return (dispatch) => {
     return getActivityTypes().then((activityType) => {
       dispatch(setActivityType(activityType))
     })
+  }
+}
+
+export function addActivityType(newActivityType) {
+  return (dispatch) => {
+    return addNewActivityType(newActivityType)
+      .then((activityType) => {
+        dispatch(setActivityTypesSuccess(activityType))
+        return null
+      })
+      .catch((err) => {
+        dispatch(setError(err.message))
+      })
+  }
+}
+
+export function setActivityTypesPending() {
+  return {
+    type: SET_ACTIVITY_TYPES_PENDING,
+  }
+}
+
+export function setActivityTypesSuccess(activityType) {
+  return {
+    type: SET_ACTIVITY_TYPES_SUCCESS,
+    activityType,
+  }
+}
+
+export function setError(errMessage) {
+  return {
+    type: SET_ERROR,
+    errMessage,
   }
 }
