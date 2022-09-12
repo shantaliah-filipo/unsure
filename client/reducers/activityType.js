dc1321af4799f5eae721fba5f1248b398448a3ae
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-12 18:17:33 +1200

    Rename to activityType

---
+++ client/reducers/activityType.js
@@ -0,0 +1,16 @@
+import { SET_ACTIVITY_TYPE } from '../actions'
+
+const initialState = []
+
+const activityTypeReducer = (state = initialState, action) => {
+  const { type, payload } = action
+  switch (type) {
+    // set_activity
+    case SET_ACTIVITY_TYPE:
+      return payload 
+    default:
+      return state
+  }
+}
+
+export default activityTypeReducer
