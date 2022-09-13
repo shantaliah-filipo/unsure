bdf17a38ac18c70bc9acddbd2cd05331c1d7ab9a
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-13 15:05:23 +1200

    First test works - testing length of new state

--- client/reducers/activityType.test.js
+++ client/reducers/activityType.test.js
@@ -1,1 +1,12 @@
-import activityTypeReducer from "./activityType";
+import activityTypeReducer from './activityType'
+import { addActivityType } from '../actions'
+
+// not working
+describe('activityType reducer', () => {
+  test('adds a new activity type', () => {
+    const action = addActivityType('running')
+    const newState = activityTypeReducer(['Lets', 'go'], action)
+    // expect(newState[2]).toBe('running')
+    expect(newState).toHaveLength(2)
+  })
+})
