e4ce3458d9e2d7659cadfe66966a310cf3f6023d
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-14 20:52:24 +1200

    Renaming routes to activity types

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
 
-const activitiesRoutes = require('./routes/activities')
+const activityTypesRoutes = require('./routes/activityTypes')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/act', activitiesRoutes)
+server.use('/api/v1/act', activityTypesRoutes)
 
 module.exports = server
