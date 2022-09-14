ce28c0bf8406ba2c57afb8b5bfa4842058f9e7ea
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-14 19:59:19 +1200

    added lines 17-22 for the test environment

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -14,18 +14,24 @@ module.exports = {
     connection: {
       filename: ':memory:',
     },
-    useNullAsDefault: true,
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
     migrations: {
-      tableName: 'knex_migrations',
+      directory: path.join(__dirname, 'migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds'),
+      useNullAsDefault: true,
+    },
+
+    production: {
+      client: 'postgresql',
+      connection: process.env.DATABASE_URL,
+      pool: {
+        min: 2,
+        max: 10,
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+      },
     },
   },
 }
